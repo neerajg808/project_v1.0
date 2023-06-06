e90b251f4ff042f5cc9567271771c93c3775fcb6
Neeraj Gupta <89775303+neerajg808@users.noreply.github.com>
2023-06-06 23:47:10 +0530

    Add files via upload
    master code Version1.0

---
+++ project/src/test/java/com/gupta/neeraj/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gupta.neeraj.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
